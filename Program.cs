4bc7c1013d78bd3b6831d507e9ecbeaa45f961e2
vinswag100 <91390530+vinswag100@users.noreply.github.com>
2022-12-06 15:01:44 +0000

    added db intializer

--- Program.cs
+++ Program.cs
@@ -15,6 +15,14 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.EnsureCreated();
+    DbInitializer.Initialize(context);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
